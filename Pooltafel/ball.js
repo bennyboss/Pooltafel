9aaef91365c92275300a016d728bcc39d5705500
RCAHaertlein <rca.haertlein@gmail.com>
2016-11-16 11:10:49 +0100

    changed lights and size balls

--- Pooltafel/ball.js
+++ Pooltafel/ball.js
@@ -5,13 +5,15 @@ class Ball extends THREE.Mesh
 {
     constructor(x,z,color)
     {
-        var bGeometry = new THREE.SphereGeometry(0.3,10,10);
+        var bGeometry = new THREE.SphereGeometry(0.5,20,20);
         var bMaterial = new THREE.MeshPhongMaterial({color: color || 0xff0000});
         super(bGeometry, bMaterial);
         borderGroup.add(this);
         this.position.x = x;
         this.position.z = z;
         this.position.y = 9;
+        this.receiveShadow = true;
+        this.castShadow = true;
     }
 
 
